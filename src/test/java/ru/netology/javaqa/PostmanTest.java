8fc8cb7261dae50074fbbd356b79b4989f7512da
Bychkova Mariya <bychk-mariya@yandex.ru>
2023-10-24 15:31:14 +0300

    initial commit

---
+++ src/test/java/ru/netology/javaqa/PostmanTest.java
@@ -0,0 +1,16 @@
+package ru.netology.javaqa;
+
+public class PostmanTest {
+    // Given - When - Then
+// Предусловия
+    given()
+  .baseUri("https://postman-echo.com")
+  .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+.when()
+  .post("/post")
+// Проверки
+.then()
+  .statusCode(200)
+  .body(/* --> ваша проверка здесь <-- */);
+}
