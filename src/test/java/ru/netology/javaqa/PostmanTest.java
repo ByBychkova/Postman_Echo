a091f18be15ca69e6f1dc45d4aa46bd98b1896b0
Bychkova Mariya <bychk-mariya@yandex.ru>
2023-10-25 18:16:16 +0300

    Finish commit

--- src/test/java/ru/netology/javaqa/PostmanTest.java
+++ src/test/java/ru/netology/javaqa/PostmanTest.java
@@ -1,21 +1,23 @@
 package ru.netology.javaqa;
+
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+
 public class PostmanTest {
     @Test
     void testPostRequestBody() {
-        // Given - When - Then
-// Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
-// Выполняемые действия
+                .contentType("text/plain; charset=UTF-8")
+                .body("0")
                 .when()
                 .post("/post")
-// Проверки
                 .then()
-                .statusCode(200);
-
+                .statusCode(200)
+                .body("data", equalTo ("0"))
+        ;
     }
 }
